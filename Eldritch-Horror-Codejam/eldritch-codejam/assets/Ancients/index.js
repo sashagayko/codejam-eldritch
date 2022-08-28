337680543a899f42d26df643a165711a691b03fc
sashagayko <95635185+sashagayko@users.noreply.github.com>
2022-08-29 02:26:57 +0300

    Delete Eldritch-Horror-Codejam/eldritch-codejam directory

--- Eldritch-Horror-Codejam/eldritch-codejam/assets/Ancients/index.js
+++
@@ -1,8 +0,0 @@
-const ancients = {
-  azathoth: './Azathoth.png',
-  cthulhu: './Cthulthu.png',
-  iogSothoth: './IogSothoth.png',
-  shubNiggurath: './ShubNiggurath.png',
-}
-
-export default ancients
